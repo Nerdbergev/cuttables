6aa4396adaaff88f95202295246bd901bb02ca0f
Janik H <Janik000@protonmail.com>
2022-06-16 02:02:36 +0200

    added object_definition

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,8 +1,34 @@
 package storage
 
 type Cuttable struct {
-	ID   int
-	Name string
+	ID          int
+	Name        string
+	Creator     int
+	Parent      int
+	Description string
+	License     int
+	Data        []byte
+}
+
+type User struct {
+	UserID   int
+	Name     string
+	Password string
+	Salt     string
+	Email    string
+}
+
+type License struct {
+	LicenseID int
+	Name      string
+	Text      string
+}
+
+type Files struct {
+	FileID     int
+	CuttableID int
+	FileType   int
+	Data       []byte
 }
 
 type CuttableStorage interface {
