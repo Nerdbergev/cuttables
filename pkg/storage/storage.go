3746d7a38831d9667bb3f453e070e86c5bcaa2af
Janik H <Janik000@protonmail.com>
2022-06-18 20:56:40 +0200

    Added tags to structs

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,34 +1,34 @@
 package storage
 
 type Cuttable struct {
-	ID          int
-	Name        string
-	Creator     int
-	Parent      int
-	Description string
-	License     int
-	Data        []byte
+	ID          int    `sql:"cuttable_id"`
+	Name        string `sql:"cuttable_name"`
+	Creator     int    `sql:"cuttable_creator"`
+	ParentID    int    `sql:"cuttable_parent_id"`
+	Description string `sql:"cuttable_description"`
+	License     int    `sql:"cuttable_license"`
+	Data        []byte `sql:"cuttable_data"`
 }
 
 type User struct {
-	UserID   int
-	Name     string
-	Password string
-	Salt     string
-	Email    string
+	ID       int    `sql:"user_id"`
+	Name     string `sql:"user_name"`
+	Password string `sql:"user_password"`
+	Salt     string `sql:"user_salt"`
+	Email    string `sql:"user_email"`
 }
 
 type License struct {
-	LicenseID int
-	Name      string
-	Text      string
+	ID   int    `sql:"license_id"`
+	Name string `sql:"license_name"`
+	Text string `sql:"license_text"`
 }
 
-type Files struct {
-	FileID     int
-	CuttableID int
-	FileType   int
-	Data       []byte
+type File struct {
+	ID         int    `sql:"file_id"`
+	CuttableID int    `sql:"file_cuttable_id"`
+	Type       int    `sql:"file_type"`
+	Data       []byte `sql:"file_data"`
 }
 
 type CuttableStorage interface {
